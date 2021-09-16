7eb7e1aefc0935b0bc80b127e2b3acb0c26ed1bd
19521635 <19521635@gm.uit.edu.vn>
2021-09-16 17:01:01 +0700

    ✨ add user profile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 # Create your models here.
 
+
 class User(AbstractUser):
-	pass
+    pass
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
+    dob = models.DateField()
+    website = models.URLField(blank=True, default="")
+    bio = models.TextField(blank=True, max_length=100, default="")
+    fb_url = models.URLField(blank=True, default="")
+    avatar = models.ImageField(default="default.jpg", upload_to="profile_imgs")
+
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'
