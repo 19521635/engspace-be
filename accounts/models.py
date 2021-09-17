3346aff62bacee05e48b113655b303f3958428c5
19521635 <19521635@gm.uit.edu.vn>
2021-09-17 17:23:22 +0700

    🎨 add property and signal to user profile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 
 
@@ -19,4 +21,19 @@ class UserProfile(models.Model):
     avatar = models.ImageField(default="default.jpg", upload_to="profile_imgs")
 
     def __str__(self):
-        return f'{self.user.first_name} {self.user.last_name}'
+        return f'{self.user.last_name} {self.user.first_name}'
+
+    @property
+    def full_name(self):
+        return f'{self.user.last_name} {self.user.first_name}'
+
+    @property
+    def username(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, *args, **kwargs):
+    if created:
+        user_profile = UserProfile(user=instance)
+        user_profile.save()
