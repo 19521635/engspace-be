bedac4de71ec76eac1967bc1d158c81d5142539d
19521635 <19521635@gm.uit.edu.vn>
2021-09-19 15:32:27 +0700

    👽️ add admin api to project

--- backend/urls.py
+++ backend/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/user/', include('accounts.urls'))
+    path("api/admin/", include('admin.urls')),
+    path('api/user/', include('accounts.urls')),
 ]
 
 if settings.DEBUG:
