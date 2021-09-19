eea53a873b49f691338ceb06e5ff0a0a775183a9
19521635 <19521635@gm.uit.edu.vn>
2021-09-19 15:31:36 +0700

    ♻️ refactor accounts url patterns

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,23 @@
 from django.urls import path
+from django.urls.conf import include
 from rest_framework_simplejwt import views as jwt_views
-from .views import UserSignUpAPIView, UserDeleteAPIView, UserProfileAPIView, UserStatusAPIView
+from .views import UserSignUpAPIView, UserProfileAPIView, UserStatusAPIView, UserFollowingAPIView
+
+follow_urlpatterns = [
+    path("", UserFollowingAPIView.as_view(), name="user_follow"),
+    path("<int:pk>", UserFollowingAPIView.as_view(),
+         name="user_follow_with_id"),
+]
+
+login_urlpatterns = [
+    path("", jwt_views.TokenObtainPairView.as_view(), name="token_create"),
+    path("refresh/", jwt_views.TokenRefreshView.as_view(), name="token_refresh"),
+]
+
 urlpatterns = [
-    path("login/", jwt_views.TokenObtainPairView.as_view(), name="token_create"),
-    path("login/refresh/", jwt_views.TokenRefreshView.as_view(), name="token_refresh"),
+    path("login/", include(login_urlpatterns)),
     path("register/", UserSignUpAPIView.as_view(), name="user_create"),
     path("profile/", UserProfileAPIView.as_view(), name="user_profile"),
     path("status/", UserStatusAPIView.as_view(), name="user_status"),
-    path("delete/<int:pk>/", UserDeleteAPIView.as_view(), name="user_delete"),
+    path("follow/", include(follow_urlpatterns)),
 ]
