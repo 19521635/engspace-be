ce072cccc948e081c57c6407e3202c00c121e9c2
19521635 <19521635@gm.uit.edu.vn>
2021-09-19 13:55:08 +0700

    🗃️ add user following model

--- accounts/models.py
+++ accounts/models.py
@@ -37,3 +37,22 @@ def create_profile(sender, instance, created, *args, **kwargs):
     if created:
         user_profile = UserProfile(user=instance)
         user_profile.save()
+
+
+class UserFollowing(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE, related_name="following")
+    following_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='followers')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'following_user'],  name="unique_followers")
+        ]
+
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f'{self.user} follows {self.following_user}'
