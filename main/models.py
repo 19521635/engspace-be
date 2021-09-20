d1fe7a5cf572ab213be8691df7e9c66ecd4e6787
19521635 <19521635@gm.uit.edu.vn>
2021-09-21 00:31:09 +0700

    🗃️ create Topic & Folder models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.conf import settings
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+
+
+class Folder(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, max_length=255, default="")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=CASCADE, related_name="folder")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, max_length=255, default="")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=CASCADE, related_name="topic")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
