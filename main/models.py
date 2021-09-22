d15d550e871444f71b83ab9d5bb39fc301913918
tqhit <abcd123huy@gmail.com>
2021-09-23 01:40:35 +0700

    🗃️ add models (folder, topic, set, set detail)

--- main/models.py
+++ main/models.py
@@ -5,19 +5,6 @@ from django.db.models.deletion import CASCADE
 # Create your models here.
 
 
-class Folder(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField(blank=True, max_length=255, default="")
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             on_delete=CASCADE, related_name="folder")
-    is_public = models.BooleanField(default=True, verbose_name="public")
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_updated = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name
-
-
 class Topic(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, max_length=255, default="")
@@ -29,3 +16,57 @@ class Topic(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Set(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, max_length=255, default="")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=CASCADE, related_name="user_set")
+    topic = models.ForeignKey(Topic, on_delete=CASCADE,
+                              related_name="topic_set")
+    is_public = models.BooleanField(default=True, verbose_name="public")
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now_add=True)
+
+
+def term_image_upload_to(instance, filename):
+    return 'term_imgs/%s/%s' % (instance.user.id, filename)
+
+
+class SetDetail(models.Model):
+    class TextLang(models.TextChoices):
+        english = "en", "English"
+        vietnamese = "vi", "Vietnamese"
+    set = models.ForeignKey(Set, on_delete=CASCADE, related_name="term")
+    image = models.ImageField(blank=True, upload_to=term_image_upload_to)
+    term = models.TextField(blank=False)
+    definition = models.TextField(blank=False)
+    term_lang = models.CharField(
+        max_length="10", choices=TextLang.choices, default=TextLang.english)
+    definition_lang = models.CharField(
+        max_length="10", choices=TextLang.choices, default=TextLang.vietnamese)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now_add=True)
+
+
+class Folder(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, max_length=255, default="")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=CASCADE, related_name="folder")
+    is_public = models.BooleanField(default=True, verbose_name="public")
+    sets = models.ManyToManyField(Set, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    def add(cls, folder, set):
+        folder.sets.add(set)
+
+    @classmethod
+    def remove(cls, folder, set):
+        folder.sets.remove(set)
