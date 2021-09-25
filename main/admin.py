bd2c8d0608da100791fecff7619fc573e19be1ce
tqhit <abcd123huy@gmail.com>
2021-09-25 18:24:42 +0700

    👽️ register set & detail models to admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
-from .models import Folder, Topic
+from .models import Folder, Topic, Set, SetDetail
 # Register your models here.
 admin.site.register(Folder)
 admin.site.register(Topic)
+
+
+class SetDetailInline(admin.StackedInline):
+    model = SetDetail
+    can_delete = True
+
+
+class SetAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description',
+                    'date_created', 'date_updated', 'user')
+    search_fields = ('id', 'name', 'user__username')
+    ordering = ('id', 'date_created', 'date_updated')
+    inlines = (SetDetailInline,)
+
+
+admin.site.register(Set, SetAdmin)
