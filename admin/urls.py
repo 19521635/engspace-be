31b6057f65255248f81b538f3b9165e9a80476a5
tqhit <abcd123huy@gmail.com>
2021-10-03 00:26:39 +0700

    👽️ update admin api

--- admin/urls.py
+++ admin/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .accounts.views import UserDeleteAPIView
+from django.urls.conf import include
 
 urlpatterns = [
-    path("user/delete/<int:pk>/", UserDeleteAPIView.as_view(),),
+    path("accounts/", include("admin.accounts.urls")),
+    path("folders/", include("admin.folders.urls")),
+    path("topics/", include("admin.topics.urls"))
 ]
