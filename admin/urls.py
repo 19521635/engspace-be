db3201dee478e86725e961f7174b8dda35686587
tqhit <abcd123huy@gmail.com>
2021-10-08 19:36:31 +0700

    👽️ change accounts api in admin api to users api

--- admin/urls.py
+++ admin/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.urls.conf import include
 
 urlpatterns = [
-    path("accounts/", include("admin.accounts.urls")),
+    path("users/", include("admin.accounts.urls")),
     path("folders/", include("admin.folders.urls")),
     path("topics/", include("admin.topics.urls")),
     path("sets/", include("admin.sets.urls")),
