48202bab4483a3bbe6eb5645da5ee7da585b1757
tqhit <abcd123huy@gmail.com>
2021-10-11 12:22:16 +0700

    👽️ add profile api to admin app

--- admin/accounts/urls.py
+++ admin/accounts/urls.py
@@ -3,5 +3,7 @@ from .views import *
 
 urlpatterns = [
     path("", UserListAPIView.as_view()),
-    path("<int:pk>/", UserDetailAPIView.as_view())
+    path("<int:pk>/", UserDetailAPIView.as_view()),
+    path("profiles/", UserProfileListAPIView.as_view()),
+    path("profiles/<int:pk>/", UserProfileDetailAPIView.as_view())
 ]
