170b1f44f4217b003144f0b0a22a285d4ea04a34
tqhit <abcd123huy@gmail.com>
2021-10-11 12:21:57 +0700

    👽️ update accounts serializers from admin app

--- admin/accounts/serializers.py
+++ admin/accounts/serializers.py
@@ -11,17 +11,22 @@ class UserListAdminSerializer(serializers.ModelSerializer):
 
 
 class UserDetailAdminSerializer(serializers.ModelSerializer):
-    dob = serializers.DateField(
-        source='profile.dob', required=False)
-    website = serializers.URLField(
-        source='profile.website', allow_blank=True, allow_null=True, required=False)
-    bio = serializers.CharField(
-        source='profile.bio', allow_blank=True, allow_null=True, required=False)
-    fb_url = serializers.URLField(
-        source='profile.fb_url', allow_blank=True, allow_null=True, required=False)
-    avatar = serializers.ImageField(source='profile.avatar', required=False)
 
     class Meta:
         model = User
         fields = "__all__"
-        extra_kwargs = {'is_superuser': {'read_only': True}}
+        extra_kwargs = {'is_superuser': {'read_only': True}, 'password': {
+            'required': False}, 'username': {'required': False}}
+
+
+class UserProfileListAdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = '__all__'
+
+
+class UserProfileDetailAdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = '__all__'
+        extra_kwargs = {'user': {'read_only': True}}
