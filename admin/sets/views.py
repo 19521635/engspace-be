9980cca2e9ec7a13ca2d415d11b26444c49c4707
tqhit <abcd123huy@gmail.com>
2021-10-11 12:55:56 +0700

    📝 update set api document on admin app

--- admin/sets/views.py
+++ admin/sets/views.py
@@ -1,4 +1,5 @@
 from rest_framework import permissions, generics
+from drf_yasg.utils import swagger_auto_schema
 from .serializers import *
 
 
@@ -8,9 +9,33 @@ class SetListAPIView(generics.ListCreateAPIView):
     queryset = Set.objects.all()
     serializer_class = SetAdminSerializer
 
+    @swagger_auto_schema(operation_summary="Retrieve a list of all sets")
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    @swagger_auto_schema(operation_summary="Save a new set")
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
 
 class SetDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     """Admin View To RetrieveUpdateDestroy Set"""
     permission_classes = (permissions.IsAdminUser,)
     queryset = Set.objects.all()
     serializer_class = SetAdminSerializer
+
+    @swagger_auto_schema(operation_summary="Retrieve a set detail for this set id")
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    @swagger_auto_schema(operation_summary="Update an existing set detail for this set id")
+    def put(self, request, *args, **kwargs):
+        return super().put(request, *args, **kwargs)
+
+    @swagger_auto_schema(operation_summary="Update partial an existing set detail for this set id")
+    def patch(self, request, *args, **kwargs):
+        return super().patch(request, *args, **kwargs)
+
+    @swagger_auto_schema(operation_summary="Delete the specified set by set id")
+    def delete(self, request, *args, **kwargs):
+        return super().delete(request, *args, **kwargs)
