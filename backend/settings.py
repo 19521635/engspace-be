01393b3fd53ce8d69f49514d614caa81fc458def
19521635 <19521635@gm.uit.edu.vn>
2021-10-22 21:41:23 +0700

    🔧 update config file to deploy

--- backend/settings.py
+++ backend/settings.py
@@ -148,6 +148,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Directory where uploaded media is saved.
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'  # Public URL at the browser
