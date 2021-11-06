5bfe3bc52b436f037fecca8eb33b88f2ec2e2887
19521635 <19521635@gm.uit.edu.vn>
2021-11-06 14:06:08 +0700

    👽️ add api get list sets/folders by user id

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
-from .views import FolderListAPIView, FolderDetailAPIView, TopicListAPIView, TopicDetailAPIView, SetListAPIView, SetDetailAPIView, FolderSetAPIVIew, SetDetailListAPIView, SetDetailDetailAPIView
+from .views import *
 
 urlpatterns = [
     path("folders/", FolderListAPIView.as_view()),
     path("folders/<int:pk>/", FolderDetailAPIView.as_view()),
+    path("folders/user/<int:pk>/", FolderListByUserAPIView.as_view()),
     path("topics/", TopicListAPIView.as_view()),
     path("topics/<int:pk>/", TopicDetailAPIView.as_view()),
     path("sets/", SetListAPIView.as_view()),
     path("sets/<int:pk>/", SetDetailAPIView.as_view()),
+    path("sets/user/<int:pk>/", SetListByUserAPIView.as_view()),
     path("set-details/", SetDetailListAPIView.as_view()),
     path("set-details/<int:pk>/", SetDetailDetailAPIView.as_view()),
     path("folder-sets/", FolderSetAPIVIew.as_view())
