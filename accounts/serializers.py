e5f63ec8dc73200f1190d95d78841864f064a3c4
19521635 <19521635@gm.uit.edu.vn>
2021-11-20 21:36:35 +0700

    🐛 fix change password api response format

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -181,16 +181,14 @@ class ChangePasswordSerializer(serializers.ModelSerializer):
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError(
-                {"password": "Password fields didn't match."})
+            raise serializers.ValidationError("Password fields didn't match.")
 
         return attrs
 
     def validate_old_password(self, value):
         user = self.context['request'].user
         if not user.check_password(value):
-            raise serializers.ValidationError(
-                {"old_password": "Old password is not correct"})
+            raise serializers.ValidationError("Old password is not correct.")
         return value
 
     def update(self, instance, validated_data):
