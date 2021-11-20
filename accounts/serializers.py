279996ea60fd796655af04a207123b593f2951ca
19521635 <19521635@gm.uit.edu.vn>
2021-11-20 22:31:07 +0700

    🐛fix do not require email in update profile api

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -46,7 +46,7 @@ class UserSignUpSerializer(serializers.ModelSerializer):
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(validators=[
+    email = serializers.EmailField(required=False, validators=[
                                    UniqueValidator(queryset=User.objects.all())])
     dob = serializers.DateField(
         source='profile.dob', required=False)
