f4de93f31d097ff50206d202638d4c728c770b2c
19521635 <19521635@gm.uit.edu.vn>
2021-11-23 13:06:53 +0700

    🍻 register forum to admin

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user',
+                    'date_created', 'date_updated', 'view_count', 'is_public')
+    search_fields = ('id', 'name', 'user__username')
+    ordering = ('id', 'is_public', 'date_created', 'date_updated')
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'post', 'parent',
+                    'date_created', 'date_updated')
+    search_fields = ('body', 'user__username', 'post__name', 'parent__id')
+    ordering = ('id', 'date_created', 'date_updated')
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
