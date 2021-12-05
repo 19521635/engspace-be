71dd0dd476170cf9a74f3ee6ce2f214152bb7f28
19521635 <19521635@gm.uit.edu.vn>
2021-12-05 23:29:55 +0700

    🔧 add email project settings

--- backend/settings.py
+++ backend/settings.py
@@ -206,3 +206,10 @@ SWAGGER_SETTINGS = {
         }
     }
 }
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
+EMAIL_USE_TLS = True
