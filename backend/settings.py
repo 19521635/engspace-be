1cd063016f3ed6e4a61a8142f30027f3de7e388f
19521635 <19521635@gm.uit.edu.vn>
2021-12-06 00:34:07 +0700

    🔧 update project settings

--- backend/settings.py
+++ backend/settings.py
@@ -215,3 +215,5 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
 EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = os.getenv(
+    "DEFAULT_FROM_EMAIL", 'Engspace Support <engspace_support@gmail.com>')
